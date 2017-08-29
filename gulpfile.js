60f8aab27482e2152f48d6dc4df2e9bd77c35339
Akshit Sawhney <akshitsawhney@cpe-192-181-2-61.kya.res.rr.com>
2017-08-29 15:06:33 +0530

    Single dist file

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,11 @@ var syntax_scss = require('postcss-scss');
 var stylelint = require('stylelint');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var clean = require('gulp-clean');
 var scssLintConfig = require('./gulp/scssLint').scssLintVar;
 var package = require('./package.json');
 
-gulp.task('default', ['scss-lint', 'sass','watch']);
+gulp.task('default', ['clean-scripts', 'scss-lint', 'sass','watch']);
 
 gulp.task("scss-lint", function() {
 
@@ -29,13 +30,18 @@ gulp.task("scss-lint", function() {
     .pipe(postcss(processors, {syntax: syntax_scss}));
 });
 
-gulp.task('sass', function () {
+gulp.task('sass', ['clean-scripts'], function () {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(rename({ suffix: '?v' + package.version }))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function () {
   gulp.watch('./scss/**/*.scss', ['sass']);
+});
+
+gulp.task('clean-scripts', function () {
+  return gulp.src('dist/*.css', {read: false})
+    .pipe(clean());
 });
